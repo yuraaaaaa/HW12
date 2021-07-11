ea70ec17df68cf1164d3cddfa24370ae30c2d1d8
yuraaaaaa <wsubota@gmail.com>
2021-07-11 07:00:39 +0300

    карточки осталось

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
-const { src, dist, parallel, series, watch } = require('gulp');
+const { src, dist, parallel, series, watch, dest } = require('gulp');
 
 const sass          = require('gulp-sass')(require('sass'));
 const browserSync   = require('browser-sync').create();
 const autoprefixer  = require('gulp-autoprefixer');
 const concat        = require('gulp-concat');
+const cleancss      = require('gulp-clean-css')
 
 function browsersync() {
     browserSync.init({
@@ -15,7 +16,10 @@ function browsersync() {
 
 function styles() {
     return src('src/scss/main.scss')
-    .pipe(concat('src/css/main.css'))
+    .pipe(sass())
+    .pipe(autoprefixer({overrideBrowserslists:['last 10 versions'], grid:true}))
+    .pipe(cleancss(({ level: { 1: {specialComments: 0}}, format:'beautify' })))
+    .pipe(dest('src/css'))
     .pipe(browserSync.stream())
     
 }
@@ -28,8 +32,14 @@ function buildcopy() {
     .pipe(dest('dist'))
 }
 
+function startwatch() {
+    watch('src/**/*.css', styles)
+    watch('src/**/*.html').on('change', browserSync.reload)
+}
+
 exports.browsersync = browsersync;
 exports.styles      = styles;
 exports.buildcopy   = buildcopy;
+exports.startwatch  = startwatch
 
 // exports.default  = 
